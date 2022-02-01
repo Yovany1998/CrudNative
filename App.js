9aa3c5982f33171109bfd3b42a970e7283eb43da
Yovany1998 <61251212+Yovany1998@users.noreply.github.com>
2022-02-01 17:09:48 -0600

    Instalacion de React Native Vector Icons

--- App.js
+++ App.js
@@ -14,20 +14,50 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Inicio from './views/Inicio';
 import NuevoCliente from './views/NuevoCliente';
 import DetallesCliente from './views/DetallesCliente';
+import Barra from './components/ui/Barra';
+
+import { DefaultTheme, Provider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
+
+//
+// Definir el tema
+const theme ={
+    ...DefaultTheme,
+    //reescribir colores
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+    accent: '#0655BF'
+}
 const App = () => {
 
   return (
    <>
     <NavigationContainer>
         <Stack.Navigator
-        initialRouteName="NuevoCliente"
+        initialRouteName="Inicio"
+        screenOptions={{
+          headerStyle:{
+          backgroundColor: theme.colors.primary
+          },
+          headerTintColor: theme.colors.surface,
+          headerTitleStyle:{
+            fontWeight: 'bold',
+          }
+        }}
         >
             <Stack.Screen
             name="Inicio"
             component={Inicio}
+            options={({navigation, route}) => ({
+              headerTitleAlign: 'center',
+              headerLeft : (props) => <Barra {...props}
+                              navigation ={navigation}
+                              route={route}
+              
+              />
+            })}
             />
 
             <Stack.Screen
---
+++ components/ui/Barra.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+
+const Barra = ({navigation,route}) => {
+
+    const handlePress =() =>{
+        navigation.navigate("NuevoCliente")
+    }
+
+  return (
+      <Button icon="plus-circle" color='#FFF' onPress={() => handlePress()}>
+          Cliente
+      </Button>
+  )
+};
+const styles = StyleSheet.create({
+   color:{
+       color: '#000'
+   }
+});
+
+export default Barra;
