aeaae8c48a1562cf4c08ed3f3379b989335e7780
Yovany1998 <61251212+Yovany1998@users.noreply.github.com>
2022-02-01 16:09:10 -0600

    Instalación de reactNavigate

---
+++ App.js
@@ -0,0 +1,60 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Inicio from './views/Inicio';
+import NuevoCliente from './views/NuevoCliente';
+import DetallesCliente from './views/DetallesCliente';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+  return (
+   <>
+    <NavigationContainer>
+        <Stack.Navigator
+        initialRouteName="NuevoCliente"
+        >
+            <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+            />
+
+            <Stack.Screen
+            name="NuevoCliente"
+            component={NuevoCliente}
+            options={{
+              title:" Nuevo Cliente"
+            }}
+            />
+            
+            <Stack.Screen
+            name="DetallesCliente"
+            component={DetallesCliente}
+            options={{
+              title:" Detalle Cliente"
+            }}
+            />
+
+      
+        </Stack.Navigator>
+    </NavigationContainer>
+   </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
---
+++ views/DetallesCliente.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const DetallesCliente = () => {
+  return (
+      <Text> desde detalle</Text>
+  )
+};
+
+export default DetallesCliente;
